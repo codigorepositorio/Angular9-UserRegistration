ce2d36e04f1591c73c0e1c0ee8ccb5bb5006324a
JuanAzure <jgutierrez_pe@outlook.com>
2020-05-22 23:08:29 -0500

    aplicativo registros de usuarios

---
+++ src/app/shared/user.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  readonly apiURL = 'http://192.168.1.49:3000/api'
+
+
+  //readonly apiURL ='http://localhost:5000/api'
+
+  formModel = this.fb.group({
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
+    FullName: [''],
+    Passwords: this.fb.group({
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword: ['', Validators.required]
+    }, { validators: this.comparePasswords })
+  });
+
+  comparePasswords(fb: FormGroup) {
+    let confirmPswrdCtrl = fb.get('ConfirmPassword');
+    //passwordMismatch
+    //confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors) {
+      if (fb.get('Password').value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+      else
+        confirmPswrdCtrl.setErrors(null);
+    }
+  }
+
+  register() {
+    var body = {
+      UserName: this.formModel.value.UserName,
+      Email: this.formModel.value.Email,
+      FullName: this.formModel.value.FullName,
+      Password: this.formModel.value.Passwords.Password
+    };
+    return this.http.post(this.apiURL + '/ApplicationUser/Register', body);
+  }
+
+  login(formData) {
+    return this.http.post(this.apiURL + '/ApplicationUser/Login', formData);
+  }
+  getUserProfile() {
+   // var tokenHeader = new HttpHeaders({ 'Authorization': 'Bearer ' + localStorage.getItem('token') });
+    return this.http.get(this.apiURL + '/UserProfile');
+  }
+}
+
+
+
---
+++ src/app/user/registration1/registration1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration1',
+  templateUrl: './registration1.component.html'
+  
+})
+export class Registration1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
